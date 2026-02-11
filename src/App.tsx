f6ad6189279c23a9d568fba186f721d89cce32ec
sudhanshu5669 <bhartiyasudhanshu5669@gmail.com>
2026-02-11 11:28:21 +0530

    Set up typescript, tailwind and LiquidEther

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import LiquidEther from './components/LiquidEther.css';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div style={{ width: '100%', height: 600, position: 'relative' }}>
+  
+</div>
+    </>
+  )
+}
+
+export default App
