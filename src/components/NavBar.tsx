3703760dc320b9063f29421b0d70b8e16d8effad
sudhanshu5669 <bhartiyasudhanshu5669@gmail.com>
2026-02-11 20:12:30 +0530

    Made a nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import BlurText from './BlurText'
 
 function NavBar() {
   return (
-    <div className='text-white relative mx-auto max-w-7xl py-4 bg-white/5 backdrop-blur-2xl rounded-4xl border border-[#2b2734]'>
-      hello
+    <div className='text-white relative mx-auto max-w-7xl pt-4 pb-4 pl-2 bg-white/5 backdrop-blur-2xl rounded-4xl border border-[#2b2734]'>
+      <BlurText
+  text="Sudhanshu Bhartiya"
+  delay={200}
+  animateBy="words"
+  direction="top"
+  className="text-2xl"
+/>
     </div>
   )
 }
