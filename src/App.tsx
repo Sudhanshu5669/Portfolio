8b40933af7ff18deaa7896fb795e192f7bd27734
sudhanshu5669 <bhartiyasudhanshu5669@gmail.com>
2026-02-11 11:35:21 +0530

    Working Liquid Ether

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import LiquidEther from './components/LiquidEther.css';
+import LiquidEther from './components/LiquidEther.tsx';
 import './App.css'
 
 function App() {
@@ -9,8 +9,24 @@ function App() {
 
   return (
     <>
-      <div style={{ width: '100%', height: 600, position: 'relative' }}>
-  
+      <div style={{ width: '100%', height: 600, position: 'relative'}} className='bg-black'>
+  <LiquidEther
+    colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
+    mouseForce={20}
+    cursorSize={100}
+    isViscous
+    viscous={30}
+    iterationsViscous={32}
+    iterationsPoisson={32}
+    resolution={0.5}
+    isBounce={false}
+    autoDemo
+    autoSpeed={0.5}
+    autoIntensity={2.2}
+    takeoverDuration={0.25}
+    autoResumeDelay={3000}
+    autoRampDuration={0.6}
+/>
 </div>
     </>
   )
