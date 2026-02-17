ef096e21e2075c4f9e9f954778ffd86fd2332e07
sudhanshu5669 <bhartiyasudhanshu5669@gmail.com>
2026-02-17 11:26:25 +0530

    TextType

--- src/App.tsx
+++ src/App.tsx
@@ -7,48 +7,59 @@ import NavBar from './components/NavBar.tsx';
 import TextType from './components/TextType.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div style={{ width: '100%', height: '100%', position: 'absolute', zIndex:-1}} className='bg-[#060111]'>
-  <LiquidEther
-    colors={[ '#5227FF', '#FF9FFC', '#B19EEF' ]}
-    mouseForce={20}
-    cursorSize={100}
-    isViscous
-    viscous={30}
-    iterationsViscous={32}
-    iterationsPoisson={32}
-    resolution={0.5}
-    isBounce={false}
-    autoDemo
-    autoSpeed={0.9}
-    autoIntensity={2.2}
-    takeoverDuration={0.25}
-    autoResumeDelay={1000}
-    autoRampDuration={0.6}
-/>
-</div>
-<div className='py-10'>
-<NavBar/>
+      {/* Background */}
+      <div
+        className="fixed inset-0 -z-10 bg-[#060111]"
+      >
+        <LiquidEther
+          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo
+          autoSpeed={0.9}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={1000}
+          autoRampDuration={0.6}
+        />
+      </div>
 
-<TextType 
-  typingSpeed={75}
-  pauseDuration={1500}
-  showCursor
-  cursorCharacter="█"
-  text={["Welcome to React Bits! Good to see you!","Build some amazing experiences!"]}
-  deletingSpeed={50}
-  variableSpeed={{ min: 60, max: 120 }}
-  cursorBlinkDuration={0.5}
-  textColors={["#ffffff"]}
-  cursorClassName="text-whites"
-/>
-</div>
+      {/* Navbar stays normal */}
+      <NavBar />
 
+      {/* Hero Section */}
+      <section className="min-h-screen flex items-center justify-center px-6 text-center">
+        <div className="max-w-4xl">
+          <TextType
+            typingSpeed={75}
+            pauseDuration={1500}
+            showCursor
+            cursorCharacter="█"
+            text={[
+              "Welcome to React Bits!",
+              "Good to see you!",
+              "Build some amazing experiences!"
+            ]}
+            deletingSpeed={50}
+            variableSpeed={{ min: 60, max: 120 }}
+            cursorBlinkDuration={0.5}
+            textColors={["#ffffff"]}
+            cursorClassName="text-white"
+            className="text-4xl md:text-6xl font-bold leading-tight tracking-tight"
+          />
+        </div>
+      </section>
     </>
   )
 }
+
 
 export default App
